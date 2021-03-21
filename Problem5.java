dfd9953ce8a746b19f0969c1deaa4032e97a9067
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-03-21 16:25:45 -0500

    Update Problem5.java

--- Problem5.java
+++ Problem5.java
@@ -1,5 +1,17 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+         int index = 0;
         
+      for ( int i = 0; i <nums.length-1;i++){
+          if (nums[i]== val){
+              index++;
+          } 
+        if (nums[i+1]>=nums.length-1){
+              if (nums[i]!=val){
+                int temp = nums[i];
+                nums[i]= nums[nums.length];
+                nums[nums.length] = temp
+      }     
+        return nums.length;
     }
 }
