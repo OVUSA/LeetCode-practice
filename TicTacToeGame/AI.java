a4e5c0547e62d18ec763b6c5964eaea8d9f62775
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-13 21:20:38 -0500

    computer player

--- TicTacToeGame/AI.java
+++ TicTacToeGame/AI.java
@@ -1,5 +1,23 @@
 package TicTacToeGame;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class AI {
 
+	public int pickSpot(TicTacToe game) {
+		ArrayList<Integer>choices = new ArrayList<>();
+		for ( int i = 0; i < 9; i++) {
+			if (game.getBoard()[i]=='-') {
+				choices.add(i+1);
+			}
+		}
+		
+		Random rand = new Random();
+		int choice = choices.get(Math.abs(rand.nextInt()%choices.size()));
+		
+		return choice;
+		
+		
+	}
 }
