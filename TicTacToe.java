b8f2ba56dc29dcf6bda55c1b30fc91fd6b9ad055
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-13 20:02:28 -0500

    added playTurn and variation of winning sets

--- TicTacToe.java
+++ TicTacToe.java
@@ -45,9 +45,56 @@ public class TicTacToe {
 		
 	}
 	
-	// create an empty board
 	public char [] setBoard() {
+		char [] board = new char[9];
+		for ( int i = 0; i < board.length;i++) {
+			board[i]= '-';
+		}	
+		return board;	
+	}
+	
+	
+	public boolean playTurn(int spot) {
+		boolean isValid = withinRange(spot) && !isSpotTaken(spot);
+		if (isValid) {
+			board[spot-1]= currentMarker;
+			// check whom turn it is. if it is userMarker then next is aiMarker and visa versa
+			currentMarker = currentMarker == userMarker ? aiMarker: userMarker;
+		}
 		
+		return isValid;
+		
+	}
+	//check if our spot within the range
+	public boolean withinRange(int spot) {
+		return (spot > 0 || spot < board.length+1)? true: false;
+		
+	}
+	
+	// check if spot is taken or empty
+	public boolean isSpotTaken(int spot){
+		return board[spot]== '-'? false: true;
+	}
+	
+	public boolean isThereWinner() {
+		boolean diagonalsAndMiddles = rightDi()|| leftDi() || middleRow()|| secondCol();
+		boolean paralleTop = topRow() || leftRow() && board[0]!= '-';
+				boolean parallelBottom = 
+	}
+	
+	public boolean rightDi() {
+		return (board[0]== 'X'&& board [4]=='X'&& board [8]=='X')||(board[0]== '0'&& board [4]=='0'&& board [8]=='0')? true: false;
+	}
+	
+	public boolean leftDi() {
+		return (board[2]== 'X'&& board [4]=='X'&& board [6]=='X')||(board[2]== '0'&& board [4]=='0'&& board [6]=='0')? true: false;
+	}
+	
+	public boolean middleRow() {
+		return (board[3]== 'X'&& board [4]=='X'&& board [5]=='X')||(board[3]== '0'&& board [4]=='0'&& board [5]=='0')? true: false;
+	}
+	public boolean secondCol() {
+		return (board[1]== 'X'&& board [4]=='X'&& board [7]=='X')||(board[1]== '0'&& board [4]=='0'&& board [7]=='0')? true: false;
 	}
 	
 	
