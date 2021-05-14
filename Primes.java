457a81b19b4de5abd0fb25fe590222cc6b767ffd
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-14 16:15:07 -0500

    find prime

--- Primes.java
+++ Primes.java
@@ -1,25 +1,18 @@
 public class Primes {
 
-	public static void main(String[] args) {
-	      Scanner sc = new Scanner (System.in);
-	      int numCases = sc.nextInt();
-	    
-	      for ( int i = 0; i < numCases;i++) {
-	    	  int num = sc.nextInt();
-	    	  primeNum (num);
-	      }
-	      
-	      
-	    }
-	
-	public static void primeNum(int num) {
-		if ( (num %2 ==0 && num!=2)|| num % 3==0 && num!=3) {
-			System.out.println("Not prime");
-			
-		}else {
-			System.out.println("Prime");
-			
-		
+public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	int numCases = sc.nextInt();
+		      
+	for ( int m = 0; m< numCases;m++){
+	int val = sc.nextInt();
+
+	for(int i=2; i<=val/i; i++){
+	if(val%i==0) val=1;
 	}
+
+	if(val==1) System.out.println("Not prime");
+	else System.out.println("Prime");
+
 	}
 }
