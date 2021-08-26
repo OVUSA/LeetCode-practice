2d05dc28a0b78e3d389dc73e121401e3dfe76015
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-08-26 14:23:02 -0500

    solution
    Sample input : 
    2
    0 2 10
    5 3 5

--- JavaLoopTwo.java
+++ JavaLoopTwo.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 public class JavaLoopTwo {
-    public static void main(String []args){
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+            
+            int num = a + (1 * b);
+            System.out.print(num + " ");
 
-            int num = a+(1*b);
-            System.out.print(num+" ");
-
-
-            for ( int k = 2; k<=n*4;k*=2){
-                num = num +(k*b);
-                System.out.print(num+ " ");
+            int temp = 2;
+            for ( int k = 0;k<n-1;k++){
+                num = num +(temp*b);
+                System.out.print(num+" ");
+                temp = temp +temp;
             }
-            System.out.println();
+            System.out.println();          
         }
         in.close();
     }
