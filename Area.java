4efd5787f10865d5be2315642f03825940181808
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-08-26 15:13:48 -0500

    static initialization block
    changes initial variable to static and added try/catch static block

--- Area.java
+++ Area.java
@@ -1,33 +1,24 @@
 import java.util.Scanner;
 
 public class Area {
-    static int B;
-    static int H;
-    static boolean flag;
-
-    public Area(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter B");
-        System.out.println("Enter H");
-
-        B = sc.nextInt();
-        H = sc.nextInt();
-        flag = print(B,H);
+    static Scanner sc = new Scanner(System.in);
+    static int B = sc.nextInt();
+    static int H = sc.nextInt();
+    static boolean flag = true;
+    
+    static{
+        try{        
+            if (B <=0 || H <=0){
+                flag = false;
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+            }
+        }catch(Exception e){
+                System.out.println(e);
+            }    
     }
-
-
-    public static boolean print(int breadth, int height ){
-        if (breadth>0 && height >0){
-            return true;
-        }else
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        return false;
-
-    }
+    
 
     public static void main(String[] args){
-
-        Area area1 = new Area();
         if(flag){
             int area=B*H;
             System.out.print(area);
