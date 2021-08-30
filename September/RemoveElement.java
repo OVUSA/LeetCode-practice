9e218b1c5da14ca622d438d4237ec2d639d99db1
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-08-30 15:36:47 -0500

    version1

--- September/RemoveElement.java
+++ September/RemoveElement.java
@@ -1,5 +1,23 @@
 public class RemoveElement {
    public static void main(String[] args){
+       int count = 0;
+        for ( int i = 0; i < nums.length-1;i++){
+            if(nums[i]== nums[i+1]){
+                count++;
+                char temp = '_';
+                for( int k = 1;k<nums.length-1;k++){
+                    nums[i] = nums[k];
+                }
+                nums[nums.length-1-i] = temp;
+                
+            }else{
+                continue;
+            }
+            
+        }
+        return nums.length;
+    }
+    
 
     }
 }
