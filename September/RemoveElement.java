393802cf2c8f5fd04d7036fd46d2f20c31c82b39
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-08-31 14:11:50 -0500

    didnt work on LeeCode
    worked on local machine

--- September/RemoveElement.java
+++ September/RemoveElement.java
@@ -1,23 +1,27 @@
 public class RemoveElement {
-   public static void main(String[] args){
-       int count = 0;
-        for ( int i = 0; i < nums.length-1;i++){
-            if(nums[i]== nums[i+1]){
-                count++;
-                char temp = '_';
-                for( int k = 1;k<nums.length-1;k++){
-                    nums[i] = nums[k];
-                }
-                nums[nums.length-1-i] = temp;
-                
-            }else{
-                continue;
-            }
-            
-        }
-        return nums.length;
-    }
-    
+      public static void main(String[] args){
+       int [] nums = {1,1,2};
+       removeDuplicates(nums);
 
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+       Set<Integer> num = new HashSet<Integer>();
+       for(int i :nums){
+           Integer i1 = new Integer(i);
+           num.add(i1);
+
+       }
+
+       for(Integer i: num){
+           nums[i] = i;
+           System.out.print(nums[i]+" ");
+       }
+
+
+
+        return num.size();
+    }
     }
 }
